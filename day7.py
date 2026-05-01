24e9a483949f4b7c93d210975a478343582dfbf9
Ishani <bharsakadeishani@gmail.com>
2026-05-01 12:19:55 +0530

    Day 7: Contains Duplicate

---
+++ day7.py
@@ -0,0 +1,6 @@
+#Contains Duplicate-Leetcode#217
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        my_set=set(nums)
+        return len(my_set)!=len(nums)
+            
