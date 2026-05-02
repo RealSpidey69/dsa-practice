37f44a308968b8e9b4e605dbad9fd9edca492dbe
Ishani <bharsakadeishani@gmail.com>
2026-05-02 07:56:57 +0530

    Day 7 Bonus Problem: Maximum Subarray

---
+++ day7_bonus.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums):
+        current_sum = 0
+        max_sum = float('-inf')
+        for i in range(len(nums)):
+            current_sum = max(nums[i], current_sum + nums[i])
+            if current_sum > max_sum:
+                max_sum = current_sum
+        return max_sum
