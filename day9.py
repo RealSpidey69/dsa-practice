12dbe9fb15e8467a5bbdeac1198973e16bcdf944
Ishani <bharsakadeishani@gmail.com>
2026-05-03 11:05:56 +0530

    Day 9:Best Day to Buy and Sell Stock II

---
+++ day9.py
@@ -0,0 +1,8 @@
+#Best Time to Buy and Sell Stock II-Leetcode#122
+class Solution:
+    def maxProfit(self, prices):
+        profit = 0
+        for i in range(len(prices) - 1):
+            if prices[i+1] > prices[i]:
+                profit += prices[i+1] - prices[i]
+        return profit
