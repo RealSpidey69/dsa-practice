a159ffa6e8071f87542c1946cff6aeab0795911a
Ishani <bharsakadeishani@gmail.com>
2026-05-05 17:29:20 +0530

    Day 11: Maximum Product Subarray

---
+++ day11.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProduct(self, nums):
+        current_max = nums[0]
+        current_min = nums[0]
+        max_product = nums[0]
+        
+        for num in nums[1:]:
+              new_max = max(num, current_max*num, current_min*num)
+              new_min = min(num, current_max*num, current_min*num)
+              current_max = new_max
+              current_min =new_min
+              max_product=max(max_product, new_max)
+        
+        return max_product
